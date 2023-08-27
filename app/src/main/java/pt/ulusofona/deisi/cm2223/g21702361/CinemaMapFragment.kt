f354bea66ac8bb8bfa05f839b74b47374583beb8
Filipe Cacho <89417564+FilipeCacho@users.noreply.github.com>
2023-08-27 12:18:30 +0100

    fixed map cached location

--- app/src/main/java/pt/ulusofona/deisi/cm2223/g21702361/CinemaMapFragment.kt
+++ app/src/main/java/pt/ulusofona/deisi/cm2223/g21702361/CinemaMapFragment.kt
@@ -1,12 +1,17 @@
 package pt.ulusofona.deisi.cm2223.g21702361
 
 import android.Manifest
+import android.content.Context
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Bundle
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -28,11 +33,15 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
-class CinemaMapFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
+class CinemaMapFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMarkerClickListener, LocationListener {
 
     private lateinit var db: AppDatabase
     private var googleMap: GoogleMap? = null
     private val LOCATION_PERMISSION_REQUEST_CODE = 1
+    private lateinit var locationManager: LocationManager
+    private var userMarker: Marker? = null // New member variable for user marker
+    private var isCameraInitialized = false
+
 
     private val requestPermissionLauncher =
         registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
@@ -49,6 +58,7 @@ class CinemaMapFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMarkerClic
         super.onViewCreated(view, savedInstanceState)
 
         db = AppDatabase.getDatabase(requireContext())
+        locationManager = requireContext().getSystemService(Context.LOCATION_SERVICE) as LocationManager
 
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
@@ -57,9 +67,10 @@ class CinemaMapFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMarkerClic
     override fun onMapReady(map: GoogleMap) {
         googleMap = map
         googleMap?.setOnMarkerClickListener(this)
-        // Enable the zoom controls
         googleMap?.uiSettings?.isZoomControlsEnabled = true
 
+        // Disable the default MyLocation layer; comment out this line
+        // googleMap?.isMyLocationEnabled = true
 
         enableUserLocation()
         loadCinemas()
@@ -68,12 +79,36 @@ class CinemaMapFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMarkerClic
     private fun enableUserLocation() {
         if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION)
             == PackageManager.PERMISSION_GRANTED) {
-            googleMap?.isMyLocationEnabled = true
+            // Comment out or remove this line to disable the default blue dot
+            // googleMap?.isMyLocationEnabled = true
+
             googleMap?.uiSettings?.isMyLocationButtonEnabled = true
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0L, 0f, this, Looper.getMainLooper())
         } else {
             requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
         }
     }
+
+
+    override fun onLocationChanged(location: Location) {
+        val userLocation = LatLng(location.latitude, location.longitude)
+
+        // If marker is null, add new marker
+        if (userMarker == null) {
+            val markerOptions = MarkerOptions().position(userLocation)
+            userMarker = googleMap?.addMarker(markerOptions)
+        } else {
+            // Update the position of the existing marker
+            userMarker?.position = userLocation
+        }
+
+        // Initialize the camera position only once
+        if (!isCameraInitialized) {
+            googleMap?.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation, 10f))
+            isCameraInitialized = true
+        }
+    }
+
 
     private fun loadCinemas() {
         viewLifecycleOwner.lifecycleScope.launch {
